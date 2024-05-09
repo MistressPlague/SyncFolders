1bf69f7030392ef6ddda63da9fa00f29fbf3b476
Mistress Plague <krewellamusicps3@gmail.com>
2024-05-09 23:49:56 +0100

    NuGet config added, now supports multiple "To" directories, added ConfigLib file to git.

--- SyncFolders/SyncFolders.Designer.cs
+++ SyncFolders/SyncFolders.Designer.cs
@@ -33,11 +33,14 @@
             this.FromThisButton = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
-            this.ToThisButton = new System.Windows.Forms.Button();
-            this.ToThisBox = new System.Windows.Forms.TextBox();
             this.DirWatcher = new System.IO.FileSystemWatcher();
             this.DirectoryDialog = new System.Windows.Forms.FolderBrowserDialog();
             this.EnabledCheckbox = new System.Windows.Forms.CheckBox();
+            this.SyncAllButton = new System.Windows.Forms.Button();
+            this.DeleteFilterCheckBox = new System.Windows.Forms.CheckBox();
+            this.ToTheseListBox = new System.Windows.Forms.ListBox();
+            this.AddButton = new System.Windows.Forms.Button();
+            this.MinusButton = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.DirWatcher)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,6 +56,7 @@
             // 
             // FromThisButton
             // 
+            this.FromThisButton.Cursor = System.Windows.Forms.Cursors.Hand;
             this.FromThisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.FromThisButton.Location = new System.Drawing.Point(243, 38);
             this.FromThisButton.Name = "FromThisButton";
@@ -74,34 +78,15 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 74);
+            this.label2.Location = new System.Drawing.Point(12, 81);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(46, 13);
+            this.label2.Size = new System.Drawing.Size(53, 13);
             this.label2.TabIndex = 5;
-            this.label2.Text = "To This:";
-            // 
-            // ToThisButton
-            // 
-            this.ToThisButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.ToThisButton.Location = new System.Drawing.Point(243, 67);
-            this.ToThisButton.Name = "ToThisButton";
-            this.ToThisButton.Size = new System.Drawing.Size(75, 23);
-            this.ToThisButton.TabIndex = 4;
-            this.ToThisButton.Text = "Browse";
-            this.ToThisButton.UseVisualStyleBackColor = true;
-            this.ToThisButton.Click += new System.EventHandler(this.ToThisButton_Click);
-            // 
-            // ToThisBox
-            // 
-            this.ToThisBox.BackColor = System.Drawing.Color.Black;
-            this.ToThisBox.ForeColor = System.Drawing.Color.Magenta;
-            this.ToThisBox.Location = new System.Drawing.Point(101, 69);
-            this.ToThisBox.Name = "ToThisBox";
-            this.ToThisBox.Size = new System.Drawing.Size(136, 20);
-            this.ToThisBox.TabIndex = 3;
+            this.label2.Text = "To These";
             // 
             // DirWatcher
             // 
+            this.DirWatcher.EnableRaisingEvents = true;
             this.DirWatcher.IncludeSubdirectories = true;
             this.DirWatcher.SynchronizingObject = this;
             this.DirWatcher.Changed += new System.IO.FileSystemEventHandler(this.DirWatcher_Changed);
@@ -109,6 +94,7 @@
             // EnabledCheckbox
             // 
             this.EnabledCheckbox.AutoSize = true;
+            this.EnabledCheckbox.Cursor = System.Windows.Forms.Cursors.Hand;
             this.EnabledCheckbox.Location = new System.Drawing.Point(15, 12);
             this.EnabledCheckbox.Name = "EnabledCheckbox";
             this.EnabledCheckbox.Size = new System.Drawing.Size(65, 17);
@@ -116,16 +102,81 @@
             this.EnabledCheckbox.Text = "Enabled";
             this.EnabledCheckbox.UseVisualStyleBackColor = true;
             // 
+            // SyncAllButton
+            // 
+            this.SyncAllButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.SyncAllButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.SyncAllButton.Location = new System.Drawing.Point(243, 12);
+            this.SyncAllButton.Name = "SyncAllButton";
+            this.SyncAllButton.Size = new System.Drawing.Size(75, 23);
+            this.SyncAllButton.TabIndex = 7;
+            this.SyncAllButton.Text = "Sync All";
+            this.SyncAllButton.UseVisualStyleBackColor = true;
+            this.SyncAllButton.Click += new System.EventHandler(this.SyncAllButton_Click);
+            // 
+            // DeleteFilterCheckBox
+            // 
+            this.DeleteFilterCheckBox.AutoSize = true;
+            this.DeleteFilterCheckBox.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DeleteFilterCheckBox.Location = new System.Drawing.Point(86, 12);
+            this.DeleteFilterCheckBox.Name = "DeleteFilterCheckBox";
+            this.DeleteFilterCheckBox.Size = new System.Drawing.Size(139, 17);
+            this.DeleteFilterCheckBox.TabIndex = 8;
+            this.DeleteFilterCheckBox.Text = "Delete Files Not In From";
+            this.DeleteFilterCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ToTheseListBox
+            // 
+            this.ToTheseListBox.BackColor = System.Drawing.Color.Black;
+            this.ToTheseListBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ToTheseListBox.ForeColor = System.Drawing.Color.Magenta;
+            this.ToTheseListBox.FormattingEnabled = true;
+            this.ToTheseListBox.Location = new System.Drawing.Point(15, 100);
+            this.ToTheseListBox.Name = "ToTheseListBox";
+            this.ToTheseListBox.Size = new System.Drawing.Size(303, 171);
+            this.ToTheseListBox.TabIndex = 9;
+            // 
+            // AddButton
+            // 
+            this.AddButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("AddButton.BackgroundImage")));
+            this.AddButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.AddButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddButton.FlatAppearance.BorderSize = 0;
+            this.AddButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddButton.Location = new System.Drawing.Point(298, 77);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(20, 20);
+            this.AddButton.TabIndex = 10;
+            this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
+            // 
+            // MinusButton
+            // 
+            this.MinusButton.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("MinusButton.BackgroundImage")));
+            this.MinusButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.MinusButton.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.MinusButton.FlatAppearance.BorderSize = 0;
+            this.MinusButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.MinusButton.Location = new System.Drawing.Point(272, 77);
+            this.MinusButton.Name = "MinusButton";
+            this.MinusButton.Size = new System.Drawing.Size(20, 20);
+            this.MinusButton.TabIndex = 11;
+            this.MinusButton.UseVisualStyleBackColor = true;
+            this.MinusButton.Click += new System.EventHandler(this.MinusButton_Click);
+            // 
             // SyncFolders
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Black;
-            this.ClientSize = new System.Drawing.Size(334, 105);
+            this.ClientSize = new System.Drawing.Size(334, 288);
+            this.Controls.Add(this.MinusButton);
+            this.Controls.Add(this.AddButton);
+            this.Controls.Add(this.ToTheseListBox);
+            this.Controls.Add(this.DeleteFilterCheckBox);
+            this.Controls.Add(this.SyncAllButton);
             this.Controls.Add(this.EnabledCheckbox);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.ToThisButton);
-            this.Controls.Add(this.ToThisBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.FromThisButton);
             this.Controls.Add(this.FromThisBox);
@@ -139,8 +190,12 @@
             ((System.ComponentModel.ISupportInitialize)(this.DirWatcher)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-
         }
+
+        private System.Windows.Forms.Button MinusButton;
+
+        private System.Windows.Forms.ListBox ToTheseListBox;
+        private System.Windows.Forms.Button AddButton;
 
         #endregion
 
@@ -148,11 +203,11 @@
         private System.Windows.Forms.Button FromThisButton;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button ToThisButton;
-        private System.Windows.Forms.TextBox ToThisBox;
         private System.IO.FileSystemWatcher DirWatcher;
         private System.Windows.Forms.FolderBrowserDialog DirectoryDialog;
         private System.Windows.Forms.CheckBox EnabledCheckbox;
+        private System.Windows.Forms.Button SyncAllButton;
+        private System.Windows.Forms.CheckBox DeleteFilterCheckBox;
     }
 }
 
--- SyncFolders/SyncFolders.cs
+++ SyncFolders/SyncFolders.cs
@@ -1,6 +1,10 @@
 ﻿using System;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Windows.Forms;
+using Libraries;
 
 namespace SyncFolders
 {
@@ -9,7 +13,47 @@ namespace SyncFolders
         public SyncFolders()
         {
             InitializeComponent();
+            
+            Instance = this;
         }
+
+        public class Configuration
+        {
+            public bool Enabled
+            { 
+                get => Instance.EnabledCheckbox.Checked;
+                set => Instance.EnabledCheckbox.Checked = value;
+            }
+
+            public bool DeleteFilter
+            {
+                get => Instance.DeleteFilterCheckBox.Checked;
+                set => Instance.DeleteFilterCheckBox.Checked = value;
+            }
+
+            public string From
+            {
+                get => Instance.FromThisBox.Text;
+                set => Instance.FromThisBox.Text = value;
+            }
+
+            public string[] To
+            {
+                get => Instance.ToTheseListBox.Items.ToArray();
+                set
+                {
+                    foreach (var item in value)
+                    {
+                        if (!Instance.ToTheseListBox.Items.Contains(item))
+                        {
+                            Instance.ToTheseListBox.Items.Add(item);
+                        }
+                    }
+                }
+            }
+        }
+
+        public static ConfigLib<Configuration> Config;
 
         private void FromThisButton_Click(object sender, EventArgs e)
         {
@@ -20,40 +64,50 @@ namespace SyncFolders
                 FromThisBox.Text = DirectoryDialog.SelectedPath;
 
                 DirWatcher.Path = FromThisBox.Text;
-                DirWatcher.EnableRaisingEvents = !string.IsNullOrWhiteSpace(DirWatcher.Path) && !string.IsNullOrWhiteSpace(FromThisBox.Text) && !string.IsNullOrWhiteSpace(ToThisBox.Text);
-            }
-        }
-
-        private void ToThisButton_Click(object sender, EventArgs e)
-        {
-            var result = DirectoryDialog.ShowDialog();
-
-            if (result == DialogResult.OK)
-            {
-                ToThisBox.Text = DirectoryDialog.SelectedPath;
+                DirWatcher.EnableRaisingEvents = !string.IsNullOrWhiteSpace(DirWatcher.Path) && !string.IsNullOrWhiteSpace(FromThisBox.Text);
             }
         }
 
         private void DirWatcher_Changed(object sender, System.IO.FileSystemEventArgs e)
         {
-            if (!EnabledCheckbox.Checked || string.IsNullOrWhiteSpace(DirWatcher.Path) || string.IsNullOrWhiteSpace(FromThisBox.Text) || string.IsNullOrWhiteSpace(ToThisBox.Text))
+            if (!EnabledCheckbox.Checked || string.IsNullOrWhiteSpace(DirWatcher.Path) || string.IsNullOrWhiteSpace(FromThisBox.Text) || ToTheseListBox.Items.Count == 0)
             {
                 return;
             }
 
-            if (Directory.GetFiles(ToThisBox.Text, "*.*", SearchOption.AllDirectories).Length == 0)
+            DoSync(e.FullPath);
+        }
+
+        private void DoSync(string pathOfChangedFile = "")
+        {
+            foreach (var dir in Config.InternalConfig.To)
             {
-                CopyFilesRecursively(FromThisBox.Text, ToThisBox.Text);
-            }
-            else
-            {
-                if (!string.IsNullOrWhiteSpace(e.FullPath) && File.Exists(e.FullPath) && File.Exists($"{ToThisBox.Text}\\{Path.GetFileName(e.FullPath)}"))
+                if (Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Length == 0)
                 {
-                    File.WriteAllBytes($"{ToThisBox.Text}\\{Path.GetFileName(e.FullPath)}", File.ReadAllBytes(e.FullPath));
+                    CopyFilesRecursively(FromThisBox.Text, dir);
+                }
+                else if (!string.IsNullOrEmpty(pathOfChangedFile))
+                {
+                    if (File.Exists(pathOfChangedFile) && File.Exists($"{dir}\\{Path.GetFileName(pathOfChangedFile)}"))
+                    {
+                        File.WriteAllBytes($"{dir}\\{Path.GetFileName(pathOfChangedFile)}", File.ReadAllBytes(pathOfChangedFile));
+                    }
+                }
+                else
+                {
+                    CopyFilesRecursively(FromThisBox.Text, dir);
+                }
+                
+                if (Config.InternalConfig.DeleteFilter)
+                {
+                    foreach (var file in Directory.GetFiles(dir, "*.*", SearchOption.AllDirectories).Where(o => !File.Exists($"{FromThisBox.Text}\\{Path.GetFileName(o)}")))
+                    {
+                        File.Delete(file);
+                    }
                 }
             }
         }
-
+        
         private static void CopyFilesRecursively(string sourcePath, string targetPath)
         {
             foreach (var dirPath in Directory.GetDirectories(sourcePath, "*", SearchOption.AllDirectories))
@@ -66,29 +120,77 @@ namespace SyncFolders
 
             foreach (var newPath in Directory.GetFiles(sourcePath, "*.*", SearchOption.AllDirectories))
             {
-                if (!File.Exists(newPath.Replace(sourcePath, targetPath)))
-                {
-                    File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
-                }
+                File.Copy(newPath, newPath.Replace(sourcePath, targetPath), true);
             }
         }
 
+        public static SyncFolders Instance;
+
         private void SyncFolders_Load(object sender, EventArgs e)
         {
+            DirWatcher.EnableRaisingEvents = false;
+            DirWatcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime |
+                                      NotifyFilters.DirectoryName | NotifyFilters.FileName |
+                                      NotifyFilters.LastWrite | NotifyFilters.Size;
+
             DirWatcher.Created += DirWatcher_Changed;
             DirWatcher.Deleted += DirWatcher_Changed;
             DirWatcher.Changed += DirWatcher_Changed;
             DirWatcher.Renamed += DirWatcher_Changed;
 
-            DirWatcher.NotifyFilter = NotifyFilters.Attributes | NotifyFilters.CreationTime |
-                                      NotifyFilters.DirectoryName | NotifyFilters.FileName |
-                                      NotifyFilters.LastWrite | NotifyFilters.Size;
+            Config = new ConfigLib<Configuration>($"{Environment.CurrentDirectory}\\{Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location)}.json");
+
+            if (Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location) != "SyncFolders")
+                Text = "SyncFolders - " + Path.GetFileNameWithoutExtension(Assembly.GetEntryAssembly().Location);
         }
 
         private void FromThisBox_TextChanged(object sender, EventArgs e)
         {
-            DirWatcher.Path = FromThisBox.Text;
-            DirWatcher.EnableRaisingEvents = !string.IsNullOrWhiteSpace(DirWatcher.Path) && !string.IsNullOrWhiteSpace(FromThisBox.Text) && !string.IsNullOrWhiteSpace(ToThisBox.Text);
+            try
+            {
+                DirWatcher.Path = FromThisBox.Text;
+                DirWatcher.EnableRaisingEvents = !string.IsNullOrWhiteSpace(DirWatcher.Path) && !string.IsNullOrWhiteSpace(FromThisBox.Text);
+            }
+            catch
+            {
+                
+            }
+        }
+
+        private void SyncAllButton_Click(object sender, EventArgs e)
+        {
+            DoSync();
+        }
+
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            var result = DirectoryDialog.ShowDialog();
+
+            if (result == DialogResult.OK)
+            {
+                ToTheseListBox.Items.Add(DirectoryDialog.SelectedPath);
+            }
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            if (ToTheseListBox.SelectedIndex == -1)
+            {
+                return;
+            }
+            
+            ToTheseListBox.Items.RemoveAt(ToTheseListBox.SelectedIndex);
+        }
+    }
+
+    public static class Extensions
+    {
+        // create ToArray for ObjectCollection
+        public static string[] ToArray(this ListBox.ObjectCollection collection)
+        {
+            var array = new string[collection.Count];
+            collection.CopyTo(array, 0);
+            return array;
         }
     }
 }
